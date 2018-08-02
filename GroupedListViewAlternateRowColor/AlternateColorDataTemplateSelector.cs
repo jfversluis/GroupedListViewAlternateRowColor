c5fb5e0720b34aeb78fef62d57759ca8653718f3
Gerald Versluis <gerald@verslu.is>
2018-08-02 09:01:46 +0200

    Initial commit

---
+++ GroupedListViewAlternateRowColor/AlternateColorDataTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using MvvmHelpers;
+using Xamarin.Forms;
+
+namespace GroupedListViewAlternateRowColor
+{
+	public class AlternateColorDataTemplateSelector : DataTemplateSelector
+	{
+		public DataTemplate EvenTemplate { get; set; }
+		public DataTemplate UnevenTemplate { get; set; }
+
+		private List<string> flatList;
+
+		protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+		{
+			if (flatList == null)
+			{
+				var groupedList = (ObservableCollection<Grouping<string, string>>)((ListView)container).ItemsSource;
+				flatList = groupedList.SelectMany(group => group).ToList();
+			}
+
+			return flatList.IndexOf(item as string) % 2 == 0 ? EvenTemplate : UnevenTemplate;
+		}
+	}
+}
---
+++ GroupedListViewAlternateRowColor/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using MvvmHelpers;
+using Xamarin.Forms;
+
+namespace GroupedListViewAlternateRowColor
+{
+	public partial class MainPage : ContentPage
+	{
+		public ObservableCollection<Grouping<string, string>> ListItems { get; set; }
+			= new ObservableCollection<Grouping<string, string>>();
+
+		public List<string> StarWarsItems { get; set; } = new List<string>
+		{
+			"R2-D2",
+			"3CPO",
+			"Luke Skywalker",
+			"Darth Vader",
+			"Han Solo",
+			"Chewbacca",
+			"Yoda",
+			"Obi-Wan Kenobi"
+		};
+
+		public List<string> StarTrekItems { get; set; } = new List<string>
+		{
+			"Jean-Luc Picard",
+			"James T. Kirk",
+			"Spock",
+			"Data",
+			"Catherine Janeway"
+		};
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+			ListItems.Add(new Grouping<string, string>("Star Wars", StarWarsItems));
+			ListItems.Add(new Grouping<string, string>("Star Trek", StarTrekItems));
+
+			BindingContext = this;
+		}
+	}
+}
